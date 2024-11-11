4d808c2033613ad5c6cdee6183565a3095877cc6
chenjianmin <chenjianmin@xunlei.com>
2024-11-11 20:21:12 +0800

    fix: 调整 prettierrc

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,11 @@
 export default {
-  printWidth: 120, // 一行最多 120 字符
-  tabWidth: 2, // 使用 2 个空格缩进
-  singleQuote: true, // 使用单引号
-  semi: false, // 行尾需要有分号
-  eslintIntegration: true,
-  stylelintIntegration: true,
+  printWidth: 120, // 超过 120 个字符换行
+  useTabs: true, // 使用 tab 缩进
+  tabWidth: 2, // 缩进 2 个字符
+  singleQuote: true, // 使用单引号，true-全部单引号、false-全部双引号
+  semi: true, // 行尾加分号
+  eslintIntegration: true, // 开启 eslint 代码格式校验
+  stylelintIntegration: true, // 开启 stylelint 代码格式校验
+  tslintIntegration: true, // 开启 tslint 代码格式校验
   htmlWhitespaceSensitivity: 'ignore' // 根据显示样式决定 html 要不要折行
 }
