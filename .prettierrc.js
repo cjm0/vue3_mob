75dc2bc41b7d26dba922d203401a2e38cc9e565f
chenjianmin <chenjianmin@xunlei.com>
2024-12-18 10:55:48 +0800

    build: prettie

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,12 @@
 export default {
-  printWidth: 120, // 超过 120 个字符换行
-  useTabs: true, // 使用 tab 缩进
-  tabWidth: 2, // 缩进 2 个字符
-  singleQuote: true, // 使用单引号，true-全部单引号、false-全部双引号
-  semi: true, // 行尾加分号
-  eslintIntegration: true, // 开启 eslint 代码格式校验
-  stylelintIntegration: true, // 开启 stylelint 代码格式校验
-  tslintIntegration: true, // 开启 tslint 代码格式校验
-  htmlWhitespaceSensitivity: 'ignore' // 根据显示样式决定 html 要不要折行
-}
+	printWidth: 120, // 超过 120 个字符换行
+	useTabs: false, // 不使用 tab 缩进，使用空格缩进
+	tabWidth: 2, // 缩进 2 个字符
+	singleQuote: true, // 使用单引号，true-单引号、false-双引号
+	semi: false, // 行尾不加分号
+
+	eslintIntegration: true, // 开启 eslint 代码格式校验
+	stylelintIntegration: true, // 开启 stylelint 代码格式校验
+	tslintIntegration: true, // 开启 tslint 代码格式校验
+	htmlWhitespaceSensitivity: 'ignore', // 根据显示样式决定 html 要不要折行
+};
